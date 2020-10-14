879ccb9681dc985917527525deac9dc80106bd0d
sberhea <sberhea@gmail.com>
2020-10-14 20:46:20 +0000

    Completed madlibs hmtl file, working on route and function for madlibs game
    Co-authored-by: kelsi-wolter <kelsi.wolter@gmail.com>

--- madlibs.py
+++ madlibs.py
@@ -41,7 +41,31 @@ def greet_person():
                            person=player,
                            compliment=compliment)
 
+@app.route('/game')
+def show_madlib_form():
+    wants_game = request.args.get("yes_game")
+    not_playing = request.args.get("no_game")
+    
+    if wants_game:
+        return render_template("game.html")
 
+    if not_playing:
+        return render_template("goodbye.html")
+    
+@app.routes('/madlib')
+def show_madlib():
+    return render_template("madlib.html")
+
+""" if wants_compliments:
+
+    player = request.args.get("person")
+    wants_compliments = request.args.get("wants_compliments")
+    
+        nice_things = sample(COMPLIMENTS, 3)
+    else:
+        nice_things = []
+    return render_template("compliments.html",
+                           compliments=nice_things, name=player)"""
 if __name__ == '__main__':
     # Setting debug=True gives us error messages in the browser and also
     # "reloads" our web app if we change the code.
